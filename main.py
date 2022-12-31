7497011bbcd3af4eea8649a8a5f82433d83d2507
daisukekun0425 <dai.04253385@gmail.com>
2022-12-31 23:00:28 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+st.title("streamlit 超入門")
+
+
+st.write("DateFrame")
+
+df = pd.DataFrame({
+    "1列目":[1,2,3,4],
+    "2列目":[10,20,30,40]
+})
+st.dataframe(df.style.highlight_max(axis=0))
